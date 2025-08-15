ba3fc4dbc2feb1d06cd9044e500d110b20c99180
Johirul <jahirulislamrayhan07@gmail.com>
2025-08-15 09:53:33 +0600

    note
    Note2.0
    
    Co-Authored-By: Rayhan <163004776+rynex00@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const geistSans = Geist({
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 
 const geistMono = Geist_Mono({
