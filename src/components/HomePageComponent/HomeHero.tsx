c6a3ecd85689a24a04410e3ec1234188d43abec6
Johirul <jahirulislamrayhan07@gmail.com>
2025-08-29 10:27:05 +0600

    Home hero test

--- src/components/HomePageComponent/HomeHero.tsx
+++ src/components/HomePageComponent/HomeHero.tsx
@@ -6,18 +6,20 @@ export default function HomeHero() {
   return (
     <div>
       <div>
-        <div className="h-[736px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center ">
-          <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+      <div className="h-[736px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center ">
+        <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
-          <BoxesCore />
-          <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
-            Tailwind
-          </h1>
-          <p className="text-center mt-2 text-neutral-300 relative z-20">
-            Framer motion is the best animation library ngl
-          </p>
-        </div>
+        <BoxesCore />
+        <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
+          Tailwind is Awesome
+        </h1>
+        <p className="text-center mt-2 text-neutral-300 relative z-20">
+          Framer motion is the best animation library ngl
+        </p>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio temporibus tenetur ratione laudantium voluptatibus voluptas non, maxime doloremque natus? Maiores consequuntur hic dignissimos ad ea magni obcaecati itaque architecto ut.</h1>
       </div>
+    </div>
     </div>
   );
 }
+  
